a1450efa958bcbbd00a52d951c00065a96c58910
Darsh Parikh <darsh.parikh@beyondata.co.in>
2021-02-19 17:42:28 +0530

    changes in settings

--- Moview/settings.py
+++ Moview/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Moview.wsgi.application'
+#WSGI_APPLICATION = 'Moview.wsgi.application'
 
 
 # Database
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'Moview.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
+        'NAME': 'Moviewdb',
+        'USER': 'moviewuser',
         'PASSWORD': '1234',
         'HOST': 'localhost'
     }
@@ -121,4 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

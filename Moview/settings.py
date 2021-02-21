ec50dc3d8ffffec9a8f8d0cc811466a68cdf2761
Bhardvaj <bhardvajlukhi306@gmail.com>
2021-02-21 20:09:10 +0530

    Added the about page

--- Moview/settings.py
+++ Moview/settings.py
@@ -77,14 +77,13 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'moviewdb',
-        'USER': 'moviewuser',
-        'PASSWORD': '1234',
-        'HOST': 'localhost'
-    }
-  
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'moviewdb',
+    #     'USER': 'moviewuser',
+    #     'PASSWORD': '1234',
+    #     'HOST': 'localhost'
+    # }
 }
 
 
--- pages/views.py
+++ pages/views.py
@@ -5,5 +5,4 @@ def index(request):
     return render(request, 'index.html')
 
 def about(request):
-    # TODO link to about page when about page is done (Bhardvaj)
-    return redirect('index')
+    return render(request, 'about.html')

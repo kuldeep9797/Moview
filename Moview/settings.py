7458231bff2ebff8e788f7f4ac00a8ecc32bc1d9
Bhardvaj <bhardvajlukhi306@gmail.com>
2021-03-19 11:17:15 +0530

    Added the new IP to allowed host list

--- Moview/settings.py
+++ Moview/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^11=*a@$5#pftmawhcp&uqajkds@ch5$y9qp&(+n+8b9m7*(al'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','moview1.herokuapp.com','20.39.35.238','localhost']
+ALLOWED_HOSTS = ['127.0.0.1','moview1.herokuapp.com','20.39.35.238','localhost','104.211.59.21']
 
 
 # Application definition
